815633ca6ef75f954767cc0e39202dfbd672421c
FAIRU <skyfairrr@gmail.com>
2024-03-17 18:21:20 +0500

    finished HW2

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+root_path = os.path.dirname(__file__)
+
+employees_csv = os.path.join(root_path, 'homework-1', 'north_data', 'employees_data.csv')
+
+customers_csv = os.path.join(root_path,  'homework-1', 'north_data', 'customers_data.csv')
+
+orders_csv = os.path.join(root_path,  'homework-1', 'north_data', 'orders_data.csv')
+
